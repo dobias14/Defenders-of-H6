021c34a9c976ae87d419d408a73462857f56708c
dobias13 <dobias13@uniba.sk>
2017-04-26 20:00:40 +0200

    Ukazka pouzitia mojho kodu
    tento form si len prezrite nestahujte si ho. 
    pokazi vam to mozno vas form ... ten som pouzival na testovanie

--- Kostra programu - logika hry/Form1.cs
+++ Kostra programu - logika hry/Form1.cs
@@ -13,14 +13,29 @@ namespace DefendersOfH6
 {
     public partial class Form1 : Form
     {
-        World w;
+        public bool build_tower;
+        public bool build_bug;
+        public bool is_start;
+
+        public World world;
+        public Graph graph;
+        List<ThinkingObject> things = new List<ThinkingObject>();
+        Graphics g;
+
+
         public Form1()
         {
             InitializeComponent();
 
-            List<ThinkingObject> o = new List<ThinkingObject>();
+            g = CreateGraphics();
+            Console.WriteLine("vonku");
+            Console.WriteLine(things.Count);
+            world = new World(ref things, 100, 500, g);
+            Console.WriteLine("juhu");
 
-            w = new World(o,100);
+            graph = new Graph(20, 20);
+
+            //List<ThinkingObject> o = new List<ThinkingObject>();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -30,13 +45,28 @@ namespace DefendersOfH6
 
         private void button1_Click(object sender, EventArgs e)
         {
-            w.zacniKolo();
-            
+            //w.zacniKolo();
+
+            Node n = graph.getNode(1, 1);
+            Node k = graph.getNode(19, 19);
+
+            is_start = true;
+
+            BasicCreature c1 = new BasicCreature(n, k, graph, 100, 100, world);
+            build_bug = false;
+            things.Add(c1);
+
+            world.zacniKolo();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            w.skonciKolo();
+            if (is_start)
+            {
+                is_start = false;
+                MessageBox.Show("PAUSE");
+                world.skonciKolo();
+            }
         }
     }
 }
