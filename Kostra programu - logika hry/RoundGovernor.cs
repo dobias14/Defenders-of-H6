d2ed028ebf95d0a771a4bc72e3d2348dd3c12aac
dobias13 <dobias13@uniba.sk>
2017-04-27 03:41:17 +0200

    Priprava na stvrtok
    No teraz som toho pridal vela naraz. (tak by sa gitovat nemalo ...)
    Tento commit sposobil ze viem uz spawnovat priserky zhladom na obtiaznost hry.
    
    Co je nepekne je ze rychlost spawnovania priser je zavislo od rychlosti hry, ale to aj ine veci ako pohyb prisiel tak aspon sme konzistentny :D . 
    
    Bude to este treba refektorovat kvoli konstantam. Ale to az po integracii.

--- Kostra programu - logika hry/RoundGovernor.cs
+++ Kostra programu - logika hry/RoundGovernor.cs
@@ -1,34 +1,176 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Drawing;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace DefendersOfH6
 {
     public class RoundGovernor
     {
-        public static  volatile bool running;
-        Graphics g;
+
+        private enum Smer {
+            Sever,Juh,Vychod,Zapad
+        }
+
+        public static volatile bool running;
+
         volatile private int sleepingTimeLength = 17;//60 fps
 
+        private volatile int timeTillNextSpawn = 0;
+        private int howManyToSpawn = 0;
+ 
+        private Graphics g;
+        private World world;
+        private Graph graph;
+        private World.Difficulty difficultyOfRound;
+
         private List<ThinkingObject> arrayOfObject;
+
 
         ICollection myCollection;
 
 
-        public RoundGovernor(bool isRunning,ref List<ThinkingObject> arrayOfObjectInGame, Graphics g)
+        public RoundGovernor(bool isRunning,ref List<ThinkingObject> arrayOfObjectInGame,
+            Graphics g, World world, Graph graph, World.Difficulty difficultyOfRound)
         {
             running = isRunning;
-            this.g = g;
-            this.arrayOfObject = arrayOfObjectInGame;
+
+            this.g = g;                   
+            this.world = world;
+            this.graph = graph;
+            this.difficultyOfRound = difficultyOfRound;
+
+            arrayOfObject = arrayOfObjectInGame;
 
             myCollection = ArrayList.Synchronized(arrayOfObject);
+
+            setParameters();
         }
+
+        private void setParameters() {
+            switch (difficultyOfRound)
+            {
+                case World.Difficulty.Easy:
+                {
+                        howManyToSpawn = 5;
+                        timeTillNextSpawn = 300;
+                        break;
+                }
+                case World.Difficulty.Regular:
+                    {
+                        howManyToSpawn = 10;
+                        timeTillNextSpawn = 250;
+                        break;
+                    }
+                case World.Difficulty.Hard:
+                    {
+                        howManyToSpawn = 15;
+                        timeTillNextSpawn = 200;
+                        break;
+                    }
+                case World.Difficulty.Insane:
+                    {
+                        howManyToSpawn = 100;
+                        timeTillNextSpawn = 100;
+                        break;
+                    }
+                case World.Difficulty.None:
+                    {
+                        break;
+                    }        
+            }
+        }
+
+        private void resetTimeTillNextSpawn() {
+            switch (difficultyOfRound)
+            {
+                case World.Difficulty.Easy:
+                    {
+                        timeTillNextSpawn = new Random().Next(200)+50;
+                        break;
+                    }
+                case World.Difficulty.Regular:
+                    {
+                        timeTillNextSpawn = new Random().Next(50) + 30;
+                        break;
+                    }
+                case World.Difficulty.Hard:
+                    {
+                        timeTillNextSpawn = new Random().Next(30) + 15;
+                        break;
+                    }
+                case World.Difficulty.Insane:
+                    {
+                        timeTillNextSpawn = new Random().Next(10) + 5;
+                        break;
+                    }
+                case World.Difficulty.None:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        // tu potrebujem funkcionalitu ku grafu graph.getWidth;graph.getHeight
+        private Node getPositionOfNewBug() {
+            Random random = new Random();
+            Smer odkialPojdeBug = (Smer) random.Next(Enum.GetNames(typeof(Smer)).Length);
+            switch (odkialPojdeBug) {
+                case Smer.Sever:
+                    {
+                        return graph.getNode(random.Next(15),0);
+                    }
+                case Smer.Juh:
+                    {
+                        return graph.getNode(random.Next(15), 0);
+                    }
+                case Smer.Vychod:
+                    {
+                        return graph.getNode(0, random.Next(15));
+                    }
+                case Smer.Zapad:
+                    {
+                        return graph.getNode(0, random.Next(15));
+                    }
+            }
+
+            return null;
+        }
+
+        private int getAmountOfBugsInArray() {
+            int howManyBugs = 0;
+            lock (myCollection.SyncRoot)
+            {
+                foreach (ThinkingObject objectinGame in arrayOfObject)
+                {
+                    if (objectinGame is ICreature) {
+                        howManyBugs++;
+                    }
+                }
+            }
+            return howManyBugs;
+        }
+
+        private void spawnCreature(Node ciel) {
+            if (getAmountOfBugsInArray() <= howManyToSpawn) {
+                if (timeTillNextSpawn <= 0)
+                {
+                    BasicCreature newBug = new BasicCreature(getPositionOfNewBug(), ciel,
+                        graph, 100, 100, world);
+                    lock (myCollection.SyncRoot)
+                    {
+                        arrayOfObject.Add(newBug);
+                    }
+                    resetTimeTillNextSpawn();
+                }
+                else
+                {
+                    timeTillNextSpawn--;
+                }
+            }
+        }
+
 
         public void nastavFPS( double pocetFPS) {
             int pocetMilisekundNa1Akciu = (int)Math.Round((1000 / pocetFPS), MidpointRounding.ToEven);          
@@ -65,6 +207,8 @@ namespace DefendersOfH6
                 //World.addOneTick();
                 Thread.Sleep(sleepingTimeLength);
 
+                spawnCreature(graph.getNode(10,10));
+
                 lock (myCollection.SyncRoot) {
                     
                     
@@ -84,6 +228,10 @@ namespace DefendersOfH6
                        
                         objectinGame.draw(g);
                     }
+                }
+                if (world.isH6ServerDead()) {
+                    //TO DO sprava ze prehral tu bude
+                    world.skonciKolo();
                 }
             }
         }
