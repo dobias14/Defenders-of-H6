aad44d0c648f807104cb9556fca94e202cdb2c66
dobias13 <dobias13@uniba.sk>
2017-04-27 03:29:24 +0200

    Priprava na stvrtok
    Mam tu prirpaveny kod ktory by sme vedeli pouzit do integracie.
    Hlavne veci v buttonklikoch

--- Kostra programu - logika hry/Form1.cs
+++ Kostra programu - logika hry/Form1.cs
@@ -1,12 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DefendersOfH6
@@ -28,14 +22,9 @@ namespace DefendersOfH6
             InitializeComponent();
 
             g = CreateGraphics();
-            Console.WriteLine("vonku");
-            Console.WriteLine(things.Count);
-            world = new World(ref things, 100, 500, g);
-            Console.WriteLine("juhu");
-
             graph = new Graph(20, 20);
 
-            //List<ThinkingObject> o = new List<ThinkingObject>();
+            world = new World(ref things, World.Difficulty.Easy, g,graph);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -45,16 +34,17 @@ namespace DefendersOfH6
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //w.zacniKolo();
 
-            Node n = graph.getNode(1, 1);
-            Node k = graph.getNode(19, 19);
+            //Node n = graph.getNode(1, 1);
+            //Node k = graph.getNode(19, 19);
 
             is_start = true;
 
-            BasicCreature c1 = new BasicCreature(n, k, graph, 100, 100, world);
-            build_bug = false;
-            things.Add(c1);
+            //BasicCreature c1 = new BasicCreature(n, k, graph, 100, 100, world);
+            //build_bug = false;
+            //things.Add(c1);
+
+            world.setFPS(60);
 
             world.zacniKolo();
         }
@@ -64,7 +54,9 @@ namespace DefendersOfH6
             if (is_start)
             {
                 is_start = false;
-                MessageBox.Show("PAUSE");
+                if (world.isH6ServerDead()) {
+                    MessageBox.Show("H6 server has been destroyed.");
+                }              
                 world.skonciKolo();
             }
         }
