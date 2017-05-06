b530cac845a778f4474aa0ee7dff57a5cbac0518
dobias13 <dobias13@uniba.sk>
2017-05-06 20:30:17 +0200

    Pridanie farbenie podla stavu
    Teraz sa prefarbuje bug podla toh ov akom je stave 
    red - dead
    green -shooting
    blue - other (mooving)

--- Integracia_april/gui/BasicCreature.cs
+++ Integracia_april/gui/BasicCreature.cs
@@ -116,12 +116,17 @@ namespace DefendersOfH6
 
         public override void draw(Graphics g)
         {
-            if (base.presentStatus.GetType() != typeof(Dying))
+            if (base.presentStatus.GetType() == typeof(Dying))
+            {
+                g.FillEllipse(Brushes.Red, position.getX(), position.getY(), 10, 10);
+            }
+            else if (base.presentStatus.GetType() == typeof(Shooting))
+            {
+                g.FillEllipse(Brushes.Green, position.getX(), position.getY(), 10, 10);
+            }
+            else
             {
                 g.FillEllipse(Brushes.Blue, position.getX(), position.getY(), 10, 10);
-            }
-            else {
-                g.FillEllipse(Brushes.Red, position.getX(), position.getY(), 10, 10);
             }
             
         }
