1a2c9bb604fbfabc6e2459c029da009be7881929
pch <p.chovancova8@gmail.com>
2017-05-26 04:32:31 +0200

    added tests for graph and node

---
+++ Teren/tests/GraphTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DefendersOfH6;
+
+namespace NodesTests
+{
+    [TestClass]
+    public class NodeTests
+    {
+
+        [TestMethod]
+        public void TestInitialGraph()
+        {
+            int width = 5, height = 5;
+            Graph graph = new Graph(width, height);
+
+            Assert.AreEqual(graph.getWidth() , width);
+            Assert.AreEqual(graph.getHeight(), height);
+            Assert.AreEqual(graph.getNode[0,0].getId(), 0);
+            Assert.AreEqual(graph.getDistance(), 10);
+        }
+
+        [TestMethod]
+        public void TestFinalTargetGraph()
+        {
+            int width = 5, height = 5;
+            Graph graph = new Graph(width, height);
+
+            Assert.AreEqual(graph.getFinalTargetLocationId, 12);
+        }
+
+        [TestMethod]
+        public void TestCreateEgeGraph()
+        {
+            int width = 2, height = 1;
+            Graph graph = new Graph(width, height);
+
+            graph.createEdge(graph.getNodes()[0,0],graph.getNodes()[0,1]);
+               
+            Assert.AreEqual(graph.getNodes()[0,0].getNeighbous.Count, 1);
+            Assert.AreEqual(graph.getNodes()[0,1].getNeighbous.Count, 1);
+        }
+
+        [TestMethod]
+        public void TestDisableEnableNodeGraph()
+        {
+            int width = 2, height = 1;
+            Graph graph = new Graph(width, height);
+
+            graph.createEdge(graph.getNodes()[0, 0], graph.getNodes()[0, 1]);
+
+            graph.disableNode(0, 0);
+            Assert.AreEqual(graph.getNodes()[0, 1].getNeighbous.Count, 0);
+
+            graph.enableNode(0, 0);
+            Assert.AreEqual(graph.getNodes()[0, 1].getNeighbous.Count, 1);
+        }
+
+    }
+}
---
+++ Teren/tests/NodeTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DefendersOfH6;
+
+namespace NodesTests
+{
+    [TestClass]
+    public class NodeTests
+    {
+
+        [TestMethod]
+        public void TestInitialNode()
+        {
+            int id = 0, x = 0, y = 0, terrain = 0;
+            Node node = new Node(id,x,y,terrain);
+
+            Assert.AreEqual(node.getId() , id);
+            Assert.AreEqual(node.getX(), x);
+            Assert.AreEqual(node.getY(), y);
+            Assert.AreEqual(node.getTerrain(), terrain);
+        }
+
+        [TestMethod]
+        public void TestSetTerrainNode()
+        {
+            int id = 0, x = 0, y = 0, terrain = 0;
+            Node node = new Node(id, x, y, terrain);
+            node.setTerrain(10);
+            Assert.AreEqual(node.getTerrain(), 10);
+        }
+
+        [TestMethod]
+        public void TestAddRemoveEdgeNode()
+        {
+            int id = 0, x = 0, y = 0, terrain = 0;
+            Node node = new Node(id, x, y, terrain);
+
+            int id2 = 1, x2 = 0, y2 = 0, terrain2 = 1;
+            Node node2 = new Node(id2, x2, y2, terrain2);
+
+            node.addEdge(node2);
+
+            Assert.AreEqual(node.getNeighbours().Count, 1);
+
+            node.removeEdgeTo(node2);
+
+
+            Assert.AreEqual(node.getNeighbour().Count, 0);
+        }
+
+        [TestMethod]
+        public void TestEnableNode()
+        {
+            int id = 0, x = 0, y = 0, terrain = 0;
+            Node node = new Node(id, x, y, terrain);
+
+            int id2 = 1, x2 = 0, y2 = 0, terrain2 = 3;
+            Node node2 = new Node(id2, x2, y2, terrain2);
+
+            Assert.IsTrue(node.isEnable());
+            Assert.IsFalse(node2.isEnable());
+        }
+
+    }
+}
